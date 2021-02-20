57314b348777ca110fdf20ec316434d8260c896a
teamdev1l <67173150+teamdev1l@users.noreply.github.com>
2021-02-20 14:27:37 +0300

    Add files via upload

---
+++ js/block.js
@@ -0,0 +1,4 @@
+let url = '/videos/savage7.mp4';
+		window.addEventListener('devtoolschange', event => {
+				window.location = url;
+    });
---
+++ js/config.js
@@ -0,0 +1,45 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 1.5, 
+
+   // Заголовок
+  title: 'dev1l',
+
+  // Социальные сети
+  social: {
+    telegram: '@YourCodeMustDie',
+    steam: 'yourcodemustdie',
+    vk: 'diefromsuicid', 
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#f5323c',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#f5323c',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(255, 0, 55, 5.0)',
+
+
+  // Цвет заголовка
+  titleColor: '#FF0000',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 0, 55, 0.6)',
+
+
+  // Текст копирайта
+  copyrightText: 'dev - dev1l',
+  // Цвет копирайта
+  copyrightTextColor: '#f5323c',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#FF0000',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.2)',
+}
